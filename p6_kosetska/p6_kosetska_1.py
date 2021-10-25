81445638eec954b18e80af1c1bb4be3197090c15
stalgovich1 <91222399+stalgovich1@users.noreply.github.com>
2021-10-25 13:28:25 +0300

    Update p6_kosetska_1.py

--- p6_kosetska/p6_kosetska_1.py
+++ p6_kosetska/p6_kosetska_1.py
@@ -1,12 +1,12 @@
-phrase1=(input('Введіть першу фразу: ')).lower()
-phrase1=''.join(c for c in phrase1 if c.isalpha())
-phrase1 = set (phrase1)
-print(phrase1)
-phrase2=(input('Введіть другу фразу: ')).lower()
-phrase2=''.join(c for c in phrase2 if c.isalpha())
-phrase2 = set (phrase2)
-print(phrase2)
-if phrase2 == phrase1 & phrase2:
+sentence1=(input('Введіть першу фразу: ')).lower()
+sentence1=''.join(c for c in sentence1 if c.isalpha())
+phrasentence1se1 = set (sentence1)
+print(sentence1)
+sentence2=(input('Введіть другу фразу: ')).lower()
+sentence2=''.join(c for c in sentence2 if c.isalpha())
+sentence2 = set (sentence2)
+print(sentence2)
+if sentence2 == sentence2 & sentence2:
     print ('Кожна з літер другої фрази зустрічається у першій')
 else:
     print ('Не кожна з літер другої фрази зустрічається у першій')
