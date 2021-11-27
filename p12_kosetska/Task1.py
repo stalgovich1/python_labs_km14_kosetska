68a979dbff96f51ad596903b95098a2f5fb5272c
stalgovich1 <91222399+stalgovich1@users.noreply.github.com>
2021-11-27 08:38:44 +0200

    Add files via upload

---
+++ p12_kosetska/Task1.py
@@ -0,0 +1,58 @@
+dirs = [
+    ( 'folder1',
+        [
+            'file1',
+            ( 'folder2', 
+                [
+                    'file2',
+                    'file3'
+                ] 
+            ),
+            ( 'folder3', 
+                [
+                    'file3', 
+                    'file4',
+                    ('folder4', ['file3'])
+                ] 
+            ),
+            'file5'
+        ]
+    )
+]
+
+
+# ВАШ КОД ТУТ
+
+def search(folder, filename):
+ try:
+  s=folder[0]+"/"
+  if filename in folder:
+   return(s+filename)
+  for i in folder:
+   if isinstance(i, list)== True:
+    if len(i) > 1:
+     recursion=search(i, filename)
+    while s != None:
+      s = s + recursion
+    return(s)
+  return True
+ except TypeError:
+  return False
+  # ПЕРЕВІРКА
+
+print(search(dirs, 'file1'))
+print(search(dirs, 'file2'))
+print(search(dirs, 'file3'))
+print(search(dirs, 'file4'))
+print(search(dirs, 'file5'))
+print(search(dirs, 'file6'))
+print(search(dirs, 'folder1'))
+
+assert search(dirs, 'file1') == ['/folder1/file1'], 'Failed test for file1'
+assert search(dirs, 'file2') == ['/folder1//folder2/file2'], 'Failed test for file2'
+assert search(dirs, 'file3') == ['/folder1//folder2/file3', '/folder1//folder3/file3', '/folder1//folder3//folder4/file3'], 'Failed test for file3'
+assert search(dirs, 'file4') == ['/folder1//folder3/file4'], 'Failed test for file4'
+assert search(dirs, 'file5') == ['/folder1/file5'], 'Failed test for file5'
+assert search(dirs, 'file6') == [], 'Failed test for file6'
+assert search(dirs, 'folder1') == [], 'Failed test for folder1'
+print('All tests good!')
